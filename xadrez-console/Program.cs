517e95270da3ce72cb01f1f04bfb8d3b9fe75236
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-02-19 19:50:33 -0300

    Criado a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -2,14 +2,25 @@
 using xadrez_console;
 using xadrez;
 
-Tabuleiro tab = new Tabuleiro(8, 8);
+//try
+//{
+//    Tabuleiro tab = new Tabuleiro(8, 8);
 
-tab.colocarPeca( new Torre(tab, Cor.Preta), new Posicao(0, 0));
-tab.colocarPeca( new Torre(tab, Cor.Preta), new Posicao(1, 3));
-tab.colocarPeca( new Rei(tab, Cor.Preta), new Posicao(2, 4));
+//    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+//    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+//    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+//    Tela.ImprimirTabuleiro(tab);
+//    Console.WriteLine();
+//}
+//catch (TabuleiroException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
 
 
+PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+Console.WriteLine(pos);
 
-Tela.ImprimirTabuleiro(tab);
-Console.WriteLine();
-
+Console.WriteLine(pos.toPosicao());
+Console.ReadLine();
